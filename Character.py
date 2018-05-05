9020ff959902927027c99e6a3dc9984162282775
oscardssmith@gmail.com <oscardssmith@gmail.com>
2018-05-05 01:17:41 -0500

    Added character and combat simualator

--- Character.py
+++ Character.py
@@ -1,8 +1,24 @@
+class stats():
+    def __init__(self, info)
+        self.str,self.con,self.dex,self.int,self.wis,self.cha,self.hp,self.ac = stats
 
+    def __str__(self):
+        print(f'''str: {self.str}
+                  con: {self.con}
+                  dex: {self.dex}
+                  int: {self.int}
+                  wis: {self.wis}
+                  cha: {self.cha}
+                  hp: {self.hp}
+                  ac: {self.ac}''')
+        
 class Character():
     def __init__(self, stats, attacks):
         self.stats = stats
         self.attacks = attacks
         
     def attack(self, target, attack):
-        target.stats[hp] -= attack.damage(target)
+        attack.execute(target)
+
+    def __str__(self):
+        print(
--- Combat_Simulator.py
+++ Combat_Simulator.py
@@ -1,1 +1,11 @@
+Adam = Character(stats(12,11,10,10,14,17,10,16},
+                 [Attack('d20+1', 'd8+1'),Attack('d20+1', 'd10+1'),])
+Oscar = Character(stats(16,14,'dex':13,'int':11,'wis':3,'cha':11, 'hp':12, 'ac':16},
+                  [Attack('d20+3', 'd8+3'),Attack('d20+1', 'd10+1'),])
+James = Character({'str':8, 'con':12,'dex':10,'int':12,'wis':17,'cha':6, 'hp':9, 'ac':12},
+                  [Attack('d20', 'd8', stat='dex', test='13-{0}')])
+Nikko = Character({'str':8, 'con':11,'dex':13,'int':18,'wis':11,'cha':6, 'hp':8, 'ac':13},
+                  [Attack('d20', 'd8', stat='dex', test='14-{0}')])
 
+characters = [Adam, Oscar, James, Nikko]
+enimies = []
