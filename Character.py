4c88dae40897a271eb101dc98fa183a001162ee5
oscardssmith@gmail.com <oscardssmith@gmail.com>
2018-05-05 02:37:32 -0500

    added pick_attack method

--- Character.py
+++ Character.py
@@ -24,10 +24,10 @@ class Character():
         attack.execute(target)
 
     def restore(self):
-        self.hp = stats.max_hp
+        self.hp = self.stats.max_hp
 
     def target(self, targets):
-        best = None
+        best_target = None
         max_dps = 0
         for target in targets:
             for attack in target.attacks:
@@ -39,9 +39,26 @@ class Character():
                         miss += 1
                 dps = hit / (hit + miss) * attack.damage.expected
                 if dps > max_dps:
-                    best = (target, attack)
+                    best_target = target
                     max_dps = dps
-        return best
+        return best_target
+
+    def pick_attack(self, targets):
+        best_attack = None
+        max_dps = 0
+        for target in targets:
+            for attack in self.attacks:
+                hit, miss = 0, 0
+                for _ in range(100):
+                    if attack.hits():
+                        hit += 1
+                    else:
+                        miss += 1
+                dps = hit / (hit + miss) * attack.damage.expected
+                if dps > max_dps:
+                    best_attack = attack
+                    max_dps = dps
+        return best_attack
         
     def __repr__(self):
         return 'Character\n'+str(self.stats)+'\n'+str(self.attacks)
